da4a85807cb2c8ee83d1e7e0fdb19a9ff364af75
german salazar <salazar.germandiego@gmail.com>
2024-06-23 00:40:37 -0400

    edit de tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), tailwind()]
+  site: "https://yermans01.github.io",
+  integrations: [icon(), tailwind()],
 });
