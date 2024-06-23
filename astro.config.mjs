566c2205567ba62e6bc47a3dd10f8c070f896e2d
german salazar <salazar.germandiego@gmail.com>
2024-06-23 01:38:53 -0400

    cambio en base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://yermans01.github.io",
+  base: "/yermans01",
   integrations: [icon(), tailwind()],
 });
